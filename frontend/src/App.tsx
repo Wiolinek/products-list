f790ff3111332754cb64ef247a2b189d413534c3
Wiola P <wiol@onet.eu>
2024-10-26 14:53:10 +0200

    remove unnecessairy files

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,11 @@
-import React from 'react';
 import Header from './components/Header';
 import ProductList from './components/ProductsList';
 
-const App: React.FC = () => {
+const App = () => {
   return (
     <>
       <Header />
-      <main className="mb-[79px]">
+      <main>
         <ProductList />
       </main>
     </>
